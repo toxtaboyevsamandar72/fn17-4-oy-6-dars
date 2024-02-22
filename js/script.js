1ff780fa36e9ccf54ac430c15e82022f1363c1db
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-02-22 20:16:46 +0500

    Homework

---
+++ js/script.js
@@ -0,0 +1,104 @@
+// 1-misol
+
+// function calc(a, b) {
+//     console.log(a*b);
+// }
+
+// calc(3, 2);
+// calc(-3, -6);
+// calc(7, 3);
+
+
+
+// 2-misol
+
+// function toSekunds(arg) {
+//     let n;
+//     n = arg * 60
+//     return n;
+// }
+
+// console.log(toSekunds(5));
+// console.log(toSekunds(3));
+// console.log(toSekunds(2));
+
+// 3-masala
+
+
+// function name(a) {
+// let n;
+// n = a+1
+// return n
+// }
+
+// console.log(name(0));
+
+
+// 4-masal
+
+// function uchburchakYuzi(a, b) {
+//     let res;
+//     res = (a*b)/2
+
+//     return res;
+// }
+
+// console.log(uchburchakYuzi(6, 7));
+
+// 5-masala
+
+// function ageToDays(age) {
+//     let n;
+//     n = age * 365;
+//     return n;
+// }
+
+// console.log(ageToDays(65));
+
+
+// 6-masala
+
+//  function name(age) {
+//         let n;
+//         n = age **3;
+//         return n;
+//     }
+
+//     console.log(name(5));
+
+// 7-masala
+
+// function firstElement(arg1, arg2, arg3) {
+    
+
+// }
+
+// 8-masala
+
+// function power(x,y) {
+//     let res;
+//     res = x*y;
+//     return res;
+// }
+
+// console.log(power(230,10));
+
+// 9-masala
+
+// function hourToSekunds(hour) {
+//     let res;
+//     res = hour*3600;
+//     return res;
+// }
+
+// console.log(hourToSekunds(2));
+
+// 10-masala
+
+// function uchinchiTomon(tomon1, tomon2) {
+//     let res;
+//     res = (tomon1+tomon2)-1;
+//     return res;
+// }
+
+// console.log(uchinchiTomon(8, 10));
